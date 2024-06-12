312514600d22e9d8b9d23fedc969ea2b6f12b0de
nalinis07 <74039361+nalinis07@users.noreply.github.com>
2024-06-12 13:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
+});
+
+const user = prompt("Enter your name");
+const myVideo= document.createElement("video");  //crate vide element
+myVideo.muted = true;  // mute by default
+
+let myStream;  //contains audio & video stream
+
+//get audio and video and store it in our variables myStream
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;  //gets stream and stores
+        addVideoStream(myVideo, stream);  //display everyones stream
+    })
+
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
+
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
